d344a9cffac1846d79756589f385b3ec61552cb5
Davide Botti <davidebotti92@gmail.com>
2023-12-24 19:11:55 +0100

    Refactor

--- webapp/src/test/kotlin/utils/Fixtures.kt
+++ webapp/src/test/kotlin/utils/Fixtures.kt
@@ -2,6 +2,6 @@ package com.springexample.utils
 
 class Fixtures {
     companion object {
-        fun readJson(path: String): String = this::class.java.getResource(path).readText()
+        fun readJson(path: String): String = this::class.java.getResource(path)!!.readText()
     }
 }
