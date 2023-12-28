0509105526946c8f5494acdf40e68d4c7ba09a34
Davide Botti <davidebotti92@gmail.com>
2023-12-28 11:16:27 +0100

    Adds almost all tests to the mongo repo

--- adapters/src/test/kotlin/adapters/MongoCustomerRepositoryTest.kt
+++ adapters/src/test/kotlin/adapters/MongoCustomerRepositoryTest.kt
@@ -2,13 +2,13 @@ package adapters
 
 import adapters.configuration.AdaptersConfiguration
 import adapters.configuration.MongoConfiguration
+import arrow.core.left
 import arrow.core.right
 import domain.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.test.context.ContextConfiguration
 import org.testcontainers.junit.jupiter.Testcontainers
 
@@ -21,16 +21,20 @@ class MongoCustomerRepositoryIntegrationTest {
     @Autowired
     private lateinit var customerRepository: CustomerRepository
 
-    @Autowired
-    private lateinit var mongoTemplate: MongoTemplate
-
-    val customer = aCustomer("3".toId(), "John Doe".toName())
-
     @Test
-    fun `insert should return customer id on success`() {
-        val result = customerRepository.insert(customer)
+    fun `insert should return customer id on success and successfully save the customer on DB, after remove, no customer exists`() {
+        val customerId = "3".toId()
+        val newCustomer = aCustomer(customerId, "John Doe".toName())
 
-        assertThat(result).isEqualTo("3".toId().right())
+        val result = customerRepository.insert(newCustomer)
+
+        assertThat(result).isEqualTo(customerId.right())
+
+        assertThat(customerRepository.findById(customerId)).isEqualTo(newCustomer.right())
+
+        assertThat(customerRepository.remove(customerId)).isEqualTo(Unit.right())
+
+        assertThat(customerRepository.findById(customerId)).isEqualTo(CustomerNotFoundError.left())
     }
 
     @Test
@@ -44,6 +48,49 @@ class MongoCustomerRepositoryIntegrationTest {
         assertThat(customerRepository.find("Davide".toName())).isEqualTo(alreadyPresentCustomer.right())
 
         assertThat(customerRepository.findById("1".toId())).isEqualTo(alreadyPresentCustomer.right())
+    }
+
+    @Test
+    fun `add destination to an existing customer`() {
+        val newDestination = Destination("Tokyo")
+        val id = "1".toId()
+
+        customerRepository.addDestination(id, newDestination)
+
+        assertThat(customerRepository.findById(id)).isEqualTo(
+            Customer(
+                id = id,
+                name = "Davide".toName(),
+                age = 31,
+                favouriteDestinations = FavouriteDestinations(
+                    listOf(
+                        Destination("Milan"),
+                        Destination("London"),
+                        newDestination
+                    )
+                )
+            ).right()
+        )
+    }
+
+    @Test
+    fun `update an existing destination`() {
+        val id = "1".toId()
+        customerRepository.updateDestination(Destination("Milan"), Destination("San Diego"), id)
+
+        assertThat(customerRepository.findById(id)).isEqualTo(
+            Customer(
+                id = id,
+                name = "Davide".toName(),
+                age = 31,
+                favouriteDestinations = FavouriteDestinations(
+                    listOf(
+                        Destination("San Diego"),
+                        Destination("London"),
+                    )
+                )
+            ).right()
+        )
     }
 
     private fun aCustomer(id: Id, name: Name): Customer {
