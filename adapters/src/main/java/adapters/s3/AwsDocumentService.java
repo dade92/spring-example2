df86a9ab4e3951d911ad3e8da545fdcdfc03f773
Davide Botti <davidebotti92@gmail.com>
2024-06-22 19:31:03 +0200

    Adds an endpoint to read text file

--- adapters/src/main/java/adapters/s3/AwsDocumentService.java
+++ adapters/src/main/java/adapters/s3/AwsDocumentService.java
@@ -4,13 +4,11 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
@@ -31,11 +29,15 @@ public class AwsDocumentService {
         amazonS3.putObject(new PutObjectRequest(bucketName, file.getOriginalFilename(), localFile));
     }
 
-    public File read(String key) {
-        S3Object amazonS3Object = amazonS3.getObject(new GetObjectRequest(bucketName, key));
-        final InputStreamReader streamReader = new InputStreamReader(amazonS3Object.getObjectContent(), StandardCharsets.UTF_8);
-        final BufferedReader reader = new BufferedReader(streamReader);
-        return null;
+    public String readTextFile(String filename) throws IOException {
+        S3Object amazonS3Object = amazonS3.getObject(new GetObjectRequest(bucketName, filename));
+        S3ObjectInputStream objectContent = amazonS3Object.getObjectContent();
+        int content;
+        StringBuilder output = new StringBuilder();
+        while ((content = objectContent.read()) != -1) {
+            output.append((char) content);
+        }
+        return output.toString();
     }
 
     private File convertMultipartFileToFile(MultipartFile file) {
--- webapp/src/main/java/webapp/DocumentController.java
+++ webapp/src/main/java/webapp/DocumentController.java
@@ -1,10 +1,14 @@
 package webapp;
 
 import adapters.s3.AwsDocumentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class DocumentController {
@@ -20,4 +24,19 @@ public class DocumentController {
         documentService.upload(file);
     }
 
+    @GetMapping("/read")
+    public ResponseEntity readDocument(@RequestParam String fileName) {
+        try {
+            String text = documentService.readTextFile(fileName);
+            return ResponseEntity.ok(new ReadResponse(text));
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+}
+
+record ReadResponse(
+    String content
+) {
 }
