c803f9dc8d80fe50a73d5bc0cba100809da21875
dbotti <davide.botti@lastminute.com>
2024-09-02 18:03:05 +0200

    Try to use DefaultAWSCredentialsProviderChain for credentials management

--- adapters/src/main/java/adapters/configuration/AwsCredentialsConfiguration.java
+++ adapters/src/main/java/adapters/configuration/AwsCredentialsConfiguration.java
@@ -2,37 +2,31 @@ package adapters.configuration;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Objects;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableConfigurationProperties(AwsProperties.class)
 public class AwsCredentialsConfiguration {
 
     @Bean
-    public AWSCredentials awsCredentials(AwsProperties awsProperties) {
-        return DefaultAWSCredentialsProviderChain.getInstance().getCredentials();
+    @Profile("prod")
+    public AWSCredentials awsCredentialsProd(AwsProperties awsProperties) {
+        DefaultAWSCredentialsProviderChain instance = DefaultAWSCredentialsProviderChain.getInstance();
+        new ProfileCredentialsProvider().refresh();
+        return instance.getCredentials();
     }
 
-    private AWSCredentials retrieveAWSCredentials(AwsProperties awsProperties) {
-        if (Objects.equals(awsProperties.sessionToken, "${SESSION_TOKEN}")) {
-            return new BasicAWSCredentials(
-                awsProperties.accessKey,
-                awsProperties.secretAccessKey
-            );
-        } else {
-            return DefaultAWSCredentialsProviderChain.getInstance().getCredentials();
-//            return new BasicSessionCredentials(
-//                awsProperties.accessKey,
-//                awsProperties.secretAccessKey,
-//                awsProperties.sessionToken
-//            );
-        }
+    @Bean
+    public AWSCredentials awsCredentialsLocal(AwsProperties awsProperties) {
+        return new BasicAWSCredentials(
+            awsProperties.accessKey,
+            awsProperties.secretAccessKey
+        );
     }
 
 }
