285e31812c4f73c32175df8d99cf78f32cb200b0
dbotti <davide.botti@lastminute.com>
2024-09-12 17:21:19 +0200

    Adds log for liveness

--- webapp/src/main/java/webapp/utils/AliveController.java
+++ webapp/src/main/java/webapp/utils/AliveController.java
@@ -1,14 +1,19 @@
 package webapp.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AliveController {
-    //appfw stuff
+
+    private Logger logger = LoggerFactory.getLogger(AliveController.class);
+
     @GetMapping("/alive")
     public ResponseEntity<?> alive() {
+        logger.info("I'm alive!");
         return ResponseEntity.ok().build();
     }
 
