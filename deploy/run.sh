c52eae42c477cf372d1b6e8ad218792405414b20
Davide Botti <davidebotti92@gmail.com>
2024-12-07 16:51:34 +0100

    Removes useless mongo express from docker compose

--- deploy/run.sh
+++ deploy/run.sh
@@ -8,10 +8,6 @@ sleep 2
 echo "Starting app..."
 docker compose up -d app
 
-#echo "Starting mongo express interface on port 8081..."
-#docker compose up -d mongo-express
-#sleep 2
-
 docker ps
 
 echo "App seems up and running. Enjoy!"
